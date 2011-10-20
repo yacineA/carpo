077ded6db6251f2886321129f04372f978a1920c
lzy5818957@gmail.com <lzy5818957@gmail.com@6a0f240a-48df-ee16-8b5b-1deb78c854d0>
2011-10-20 23:55:20 +0000

    import android project

---
+++ trunk/android/src/com/zunyi/carpo/CarpoActivity.java
@@ -0,0 +1,13 @@
+package com.zunyi.carpo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CarpoActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
