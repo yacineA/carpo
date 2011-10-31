6849c61678f6610ad8a86c5748566747b567080a
lzy5818957@gmail.com <lzy5818957@gmail.com@6a0f240a-48df-ee16-8b5b-1deb78c854d0>
2011-10-31 19:22:35 +0000

    facebook login

--- trunk/android/gen/com/zunyi/carpo/R.java
+++ trunk/android/gen/com/zunyi/carpo/R.java
@@ -11,7 +11,14 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int close=0x7f020000;
+        public static final int facebook_icon=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+    }
+    public static final class id {
+        public static final int cancelButton=0x7f050002;
+        public static final int loginButton=0x7f050001;
+        public static final int textView1=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -19,5 +26,8 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int login_label=0x7f040002;
+        public static final int password=0x7f040004;
+        public static final int username=0x7f040003;
     }
 }
--- trunk/android/src/com/zunyi/carpo/CarpoActivity.java
+++ trunk/android/src/com/zunyi/carpo/CarpoActivity.java
@@ -1,13 +1,96 @@
 package com.zunyi.carpo;
 
+import com.facebook.android.*;
+import com.facebook.android.Facebook.*;
+
 import android.app.Activity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
 
 public class CarpoActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+	Facebook facebook = new Facebook("258295044208268");
+	//AsyncFacebookRunner mAsyncRunner = new AsyncFacebookRunner(facebook);
+	String FILENAME = "AndroidSSO_data";
+	private SharedPreferences mPrefs;
+
+	Button loginButton = null;
+	Button cancelButton = null;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		loginButton = (Button) findViewById(R.id.loginButton);
+		cancelButton = (Button) findViewById(R.id.cancelButton);
+
+		loginButton.setOnClickListener(new LoginButtonListener());
+
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		facebook.authorizeCallback(requestCode, resultCode, data);
+	}
+
+	class LoginButtonListener implements OnClickListener {
+
+		@Override
+		public void onClick(View v) {
+
+			/*
+			 * Get existing access_token if any
+			 */
+			mPrefs = getPreferences(MODE_PRIVATE);
+			String access_token = mPrefs.getString("access_token", null);
+			long expires = mPrefs.getLong("access_expires", 0);
+			if (access_token != null) {
+				facebook.setAccessToken(access_token);
+			}
+			if (expires != 0) {
+				facebook.setAccessExpires(expires);
+			}
+
+			/*
+			 * Only call authorize if the access_token has expired.
+			 */
+			if (!facebook.isSessionValid()) {
+
+				facebook.authorize(CarpoActivity.this, new String[] {}, new DialogListener() {
+					@Override
+					public void onComplete(Bundle values) {
+						System.out.println(values.getString("response"));
+						SharedPreferences.Editor editor = mPrefs.edit();
+						editor.putString("access_token", facebook.getAccessToken());
+						editor.putLong("access_expires",
+								facebook.getAccessExpires());
+						editor.commit();
+					}
+
+					@Override
+					public void onFacebookError(FacebookError error) {
+					}
+
+					@Override
+					public void onError(DialogError e) {
+					}
+
+					@Override
+					public void onCancel() {
+					}
+				});
+			}
+		}
+
+	}
 }
