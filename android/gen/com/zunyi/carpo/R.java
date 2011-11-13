4a50a4accb3a036fb40266369e2e68b2cb310600
lzy5818957 <lzy5818957@gmail.com>
2011-11-13 02:35:17 +0000

    add main menu activity

--- android/gen/com/zunyi/carpo/R.java
+++ android/gen/com/zunyi/carpo/R.java
@@ -16,18 +16,20 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int cancelButton=0x7f050002;
         public static final int loginButton=0x7f050001;
+        public static final int skipButton=0x7f050002;
         public static final int textView1=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int suggestion=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
         public static final int login_label=0x7f040002;
         public static final int password=0x7f040004;
+        public static final int suggestion_screen_title=0x7f040005;
         public static final int username=0x7f040003;
     }
 }
